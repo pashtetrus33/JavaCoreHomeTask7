2a2afb3245ccf1175889937d61fc22d72e96bee4
pashtetrus33 <pashtetrus@gmail.com>
2021-09-19 21:58:06 -0300

    hw7

--- src/main/java/WeatherResponse.java
+++ src/main/java/WeatherResponse.java
@@ -1,45 +1,94 @@
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Date;
+import java.util.List;
 
-public class WeatherResponse {
-    private String city;
-    private String localDate;
-    private double temperature;
-
-    public WeatherResponse(String city, String localDate, double temperature) {
-        this.city = city;
-        this.localDate = localDate;
-        this.temperature = temperature;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getLocalDate() {
-        return localDate;
-    }
-
-    public void setLocalDate(String localDate) {
-        this.localDate = localDate;
-    }
-
-    public double getTemperature() {
-        return temperature;
-    }
-
-    public void setTemperature(double temperature) {
-        this.temperature = temperature;
-    }
-
-    @Override
-    public String toString() {
-        return "WeatherResponse{" +
-                "city='" + city + '\'' +
-                ", localDate='" + localDate + '\'' +
-                ", temperature=" + String.format("%.2f", temperature) +
-                '}';
-    }
+class Headline{
+    @JsonProperty("EffectiveDate")
+    public Date effectiveDate;
+    @JsonProperty("EffectiveEpochDate")
+    public int effectiveEpochDate;
+    @JsonProperty("Severity")
+    public int severity;
+    @JsonProperty("Text")
+    public String text;
+    @JsonProperty("Category")
+    public String category;
+    @JsonProperty("EndDate")
+    public Date endDate;
+    @JsonProperty("EndEpochDate")
+    public int endEpochDate;
+    @JsonProperty("MobileLink")
+    public String mobileLink;
+    @JsonProperty("Link")
+    public String link;
 }
+
+class Minimum{
+    @JsonProperty("Value")
+    public double value;
+    @JsonProperty("Unit")
+    public String unit;
+    @JsonProperty("UnitType")
+    public int unitType;
+}
+
+class Maximum{
+    @JsonProperty("Value")
+    public double value;
+    @JsonProperty("Unit")
+    public String unit;
+    @JsonProperty("UnitType")
+    public int unitType;
+}
+
+class Temperature{
+    @JsonProperty("Minimum")
+    public Minimum minimum;
+    @JsonProperty("Maximum")
+    public Maximum maximum;
+}
+
+class Day{
+    @JsonProperty("Icon")
+    public int icon;
+    @JsonProperty("IconPhrase")
+    public String iconPhrase;
+    @JsonProperty("HasPrecipitation")
+    public boolean hasPrecipitation;
+}
+class Night{
+    @JsonProperty("Icon")
+    public int icon;
+    @JsonProperty("IconPhrase")
+    public String iconPhrase;
+    @JsonProperty("HasPrecipitation")
+    public boolean hasPrecipitation;
+}
+class DailyForecast{
+    @JsonProperty("Date")
+    public Date date;
+    @JsonProperty("EpochDate")
+    public int epochDate;
+    @JsonProperty("Temperature")
+    public Temperature temperature;
+    @JsonProperty("Day")
+    public Day day;
+    @JsonProperty("Night")
+    public Night night;
+    @JsonProperty("Sources")
+    public List<String> sources;
+    @JsonProperty("MobileLink")
+    public String mobileLink;
+    @JsonProperty("Link")
+    public String link;
+
+}
+
+class Root{
+    @JsonProperty("Headline")
+    public Headline headline;
+    @JsonProperty("DailyForecasts")
+    public List<DailyForecast> dailyForecasts;
+}
+
