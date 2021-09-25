5424694d70df188927301e9425d26804531ecf5c
pashtetrus33 <pashtetrus@gmail.com>
2021-09-25 19:12:21 -0300

    homwWork 7 updated

--- src/main/java/HomeTask7.java
+++ src/main/java/HomeTask7.java
@@ -2,30 +2,18 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class HomeTask7 {
-        public static void main(String[] args) {
-            AccuWeatherType accuweatherType = new AccuWeatherType();
-               Scanner scanner = new Scanner(System.in);
 
-                while (true) {
+    public static void main(String[] args) {
+        UserInterfaceView userInterfaceView = new UserInterfaceView();
+        userInterfaceView.runInterface();
+    }
+}
 
-                    System.out.println("""
-                            Введите 1 для получения погоды в Санкт-Петербурге на сегодня;
-                            Введите 5 для получения погоды в Санкт-Петербурге на 5 дней;
-                            Для выхода введите 0:""");
 
-                    String command = scanner.nextLine();
-                    if((!(command.equals("1")))&&(!(command.equals("5")))&&(!(command.equals("0")))){
-                        System.out.println("Неверный номер команды!");
-                        break;
-                    }
-                    if (command.equals("0")) break;
 
-                    try {
-                        accuweatherType.getWeather(command);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
+
+
+
+
+
 
---
+++ src/main/java/UserInterfaceView.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class UserInterfaceView {
+    private Controller controller = new Controller();
+
+    public void runInterface() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Введите имя города: ");
+            String city = scanner.nextLine();
+
+            System.out.println("Введите 1 для получения погоды на сегодня;" +
+                    "Введите 5 для прогноза на 5 дней; Введите 2 для получения данных из базы; Для выхода введите 0:");
+
+            String command = scanner.nextLine();
+
+            if((!(command.equals("1")))&&(!(command.equals("5")))&&(!(command.equals("0")))){
+                System.out.println("Неверный номер команды!");
+                break;
+            }
+            if (command.equals("0")) break;
+
+
+            try {
+                controller.getWeather(command, city);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
